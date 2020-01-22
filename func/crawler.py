3320b06875b385b3e5260763e872410c90ea67c2
Leônidas do Nascimento <leonidas.nascimento@accenture.com>
2020-01-22 10:14:33 -0300

    Adjustments on data model

--- func/crawler.py
+++ func/crawler.py
@@ -6,7 +6,7 @@ import requests
 
 from typing import List
 from datetime import (date, datetime)
-from .model.finacial_history import (FinancialHistory, History)
+from .model.finacial_history import (FinancialHistory, History, Row, Period)
 from .parser import Parser
 from bs4 import (BeautifulSoup, Tag)
 
@@ -16,18 +16,19 @@ FIELD_NET_INCOME = "Lucro Líquido"
 FIELD_NET_WORTH = "Patrimônio Líquido Total"
 FIELD_TOTAL_DEBITS = "Total de Passivos"
 
+
 class Crawler():
     url_balance_sheet: str = ""
     url_cash_flow: str = ""
     dt_processing: str = ""
     financial_history: FinancialHistory = None
     request_headers = {
-            'upgrade-insecure-requests': "1",
-            'user-agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36",
-            'accept': "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3",
-            'cache-control': "no-cache",
-            'postman-token': "146ba02b-dc25-fb1a-9fbc-a8df1248db26"
-        }
+        'upgrade-insecure-requests': "1",
+        'user-agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36",
+        'accept': "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3",
+        'cache-control': "no-cache",
+        'postman-token': "146ba02b-dc25-fb1a-9fbc-a8df1248db26"
+    }
 
     def __init__(self, _url_cash_flow: str, _url_balance_sheet: str, _dt_processing: str):
         self.url_balance_sheet = _url_balance_sheet
@@ -37,28 +38,27 @@ class Crawler():
 
     def get_data(self, _stock_code: str) -> FinancialHistory:
         self.financial_history = FinancialHistory(_stock_code, self.dt_processing)
-        
-        # Cash flow
-        self.financial_history.cash_flow.extend(self.__setHistoryDate(FIELD_SHARED_DIVIDENDS,_stock_code, self.url_cash_flow, True))
-        self.financial_history.cash_flow.extend(self.__setHistoryDate(FIELD_NET_INCOME, _stock_code, self.url_cash_flow, False))
-        
-        # Balance Sheet
-        self.financial_history.balance_sheet.extend(self.__setHistoryDate(FIELD_NET_WORTH, _stock_code, self.url_balance_sheet, False))
-        self.financial_history.balance_sheet.extend(self.__setHistoryDate(FIELD_TOTAL_DEBITS, _stock_code, self.url_balance_sheet, False))
 
-        return self.financial_history
+        cash_flow_rows: List[str] = [[FIELD_SHARED_DIVIDENDS, True], [FIELD_NET_INCOME, False]]
+        balance_sheet_rows: List[str] = [[FIELD_NET_WORTH, False], [FIELD_TOTAL_DEBITS, False]]
+
+        self.financial_history.history.append(self.__get_history("Fluxo de Caixa", cash_flow_rows, _stock_code, self.url_cash_flow, True))
+        self.financial_history.history.append(self.__get_history("Balanço", balance_sheet_rows, _stock_code, self.url_balance_sheet, False))
         
-    def __setHistoryDate(self, _description: str, _stock_code: str, url: str, _turn_value_positive: bool):
+        return self.financial_history
+
+    def __get_history(self, _hist_description: str, _rows: List[str], _stock_code: str, url: str, _turn_value_positive: bool) -> History:
         res = requests.get(url.format(_stock_code), headers=self.request_headers)
         page = BeautifulSoup(res.content)
-        return_lst: List[History] = []
+        hist_aux: History = History(_hist_description)
+        hist_aux.periods = []
 
         if not page:
             return
-    
-        period_index: int = -1 
 
-        # gettin' the periods/
+        period_index: int = -1
+
+        # gettin' the periods
         header_section: Tag = page.find("div", attrs={"class": "D(tbr) C($primaryColor)", "data-reactid": "32"})
 
         if header_section is None:
@@ -79,40 +79,58 @@ class Crawler():
                 _date_aux = text_aux.split("/")
 
                 if not (_date_aux is None or _date_aux == "" or len(_date_aux) < 3):
-                    period = date(int(_date_aux[2]), int(_date_aux[1]), int(_date_aux[0])).isoformat()
-                    
-                    # getting the values
-                    value_row: Tag = page.find("span", text=re.compile("^{}$".format(_description), re.IGNORECASE))
-                    if value_row is None:
-                        continue
+                    period_date: date = date(int(_date_aux[2]), int(_date_aux[1]), int(_date_aux[0])).isoformat()
+                    period_obj: Period = Period(period_date)
 
-                    value_cell: List[Tag] = list(value_row.parent.parent.parent.children)
-                    if value_cell is None or len(value_cell) == 0:
-                        continue
+                    for row in _rows:
+                        desc: str = row[0]
+                        turn_positive: bool = (row[1] if row[1] else False) if len(row) > 1 else False  
 
-                    if value_cell[period_index] is None:
-                        continue
-
-                    value_span: Tag = value_cell[period_index].find("span")
-                    if value_span is None:
-                        continue
-
-                    formatted_value_aux = 0.00
-
-                    if _turn_value_positive is True:
-                        formatted_value_aux = (Parser.ParseFloat(value_span.get_text()))
-
-                        if (formatted_value_aux < 0):
-                            formatted_value_aux = formatted_value_aux * -1
-                            pass
-                    else:
-                        formatted_value_aux = Parser.ParseFloat(value_span.get_text())
+                        # Row
+                        period_obj.rows.append(self.__get_row(page, period_index, desc, turn_positive))
                         pass
-
-                    hist_data: History = History(_description, period, (formatted_value_aux * 1000))
-                    return_lst.append(hist_data)
+                    
+                    # Period
+                    hist_aux.periods.append(period_obj)
                     pass
                 pass
-            continue        
-        return return_lst
+            pass 
+        return hist_aux
+
+    def __get_row(self, _page: BeautifulSoup, _period_index: int, _description: str, _turn_value_positive: bool) -> Row:
+        if not _page:
+            return None
+
+        # getting the values
+        value_row: Tag = _page.find("span", text=re.compile(
+            "^{}$".format(_description), re.IGNORECASE))
+        
+        if value_row is None:
+            return None
+
+        value_cell: List[Tag] = list(value_row.parent.parent.parent.children)
+        if value_cell is None or len(value_cell) == 0:
+            return None
+
+        if value_cell[_period_index] is None:
+            return None
+
+        value_span: Tag = value_cell[_period_index].find("span")
+        if value_span is None:
+            return None
+
+        formatted_value_aux = 0.00
+
+        if _turn_value_positive is True:
+            formatted_value_aux = (Parser.ParseFloat(value_span.get_text()))
+
+            if (formatted_value_aux < 0):
+                formatted_value_aux = formatted_value_aux * -1
+                pass
+        else:
+            formatted_value_aux = Parser.ParseFloat(value_span.get_text())
+            pass
+
+        return_row: Row = Row(_description, (formatted_value_aux * 1000))
+        return return_row
     pass
--- func/model/finacial_history.py
+++ func/model/finacial_history.py
@@ -1,28 +1,44 @@
 from datetime import datetime
 from typing import List
 
-class History():
+class Row():
     description: str = ""
-    date: datetime.date = None
     value: float = 0.00
 
-    def __init__(self, _desc:str, _date: datetime.date, _value: float):
-        self.date = _date
+    def __init__(self, _desc:str,  _value: float):
         self.value = _value
         self.description = _desc
+        pass   
+    pass
+
+class Period():
+    date: datetime.date = None
+    rows: List[Row] = []
+    
+    def __init__(self, _date: datetime.date):
+        self.date = _date
+        self.rows = []
+        pass
+    pass
+
+class History():
+    description: str = ''
+    periods: List[Period] =[]
+
+    def __init__(self, _desc: str):
+        self.description = _desc
+        self.periods = []
         pass
     pass
 
 class FinancialHistory():
     code: str = ""
     dt_processing: str = "" 
-    balance_sheet: List[History] = []
-    cash_flow: List[History] = []
+    history: List[History] = []
     
     def __init__(self, _code: str, _dt_processing: str):
         self.code = _code
         self.dt_processing = _dt_processing
-        self.cash_flow = []
-        self.balance_sheet = []
+        self.history = []
         pass
     pass
